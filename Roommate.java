fb9cef95f661b18fa2f077e93da7525ee0553836
klchen <klchen@email.wm.edu>
2022-03-26 19:40:28 -0400

    Finished final boss

--- Roommate.java
+++ Roommate.java
@@ -2,15 +2,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Roommate extends Battler{
-    public TutorialBattle(Character player_to_set, ArrayList<Enemy> enemy_to_set, Scanner in){
+    public Roommate(Character player_to_set, ArrayList<Enemy> enemy_to_set, Scanner in){
         super(player_to_set, enemy_to_set, in);
         this.player = player_to_set;
         this.enemy_list = enemy_to_set;
         say_lines = new ArrayList<String>();
-        say_lines.add("Prepare to be executed!");
-        say_lines.add("Time for a throw down!");
-        say_lines.add("Catch this!");
-        say_lines.add("Fine, I'll commit my all to this!");
-        say_lines.add("No exceptions!");
+        say_lines.add("Too bad, roomie.");
+        say_lines.add("Heh, it's not personnel.");
+        say_lines.add("Too easy!");
+        say_lines.add("Heh.");
+        say_lines.add("I won't lose here, roomie!");
         this.start_battle(in);
+    }
 }
