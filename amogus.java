842df59ed2f9551ebce6a9633974a8d75771ae28
Max Fonss <mjfonss@email.wm.edu>
2022-03-26 20:43:44 -0400

    yum

--- amogus.java
+++ amogus.java
@@ -7,7 +7,37 @@ import java.io.IOException;
 
 public class amogus {
     static JLabel jLabel = new JLabel();
-    public static void main(String[] args) throws IOException {
+    
+
+    public static void changeImage(String image) throws IOException
+    {
+        String imageDirectory = "images/" + image;
+        File file = new File(imageDirectory);
+        BufferedImage bufferedImage = ImageIO.read(file);
+        ImageIcon imageIcon = new ImageIcon(bufferedImage);
+        jLabel.setIcon(imageIcon); 
+    }
+    
+    // public static synchronized void playSound(final String url) {
+    //     new Thread(new Runnable() {
+    //     // The wrapper thread is unnecessary, unless it blocks on the
+    //     // Clip finishing; see comments.
+    //       public void run() {
+    //         try {
+    //           Clip clip = AudioSystem.getClip();
+    //           AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+    //             Main.class.getResourceAsStream("amogus.wav"));
+    //           clip.open(inputStream);
+    //           clip.start(); 
+    //         } catch (Exception e) {
+    //           System.err.println(e.getMessage());
+    //         }
+    //       }
+    //     }).start();
+    //   }
+
+      public static void main(String[] args) throws IOException {
+        // playSound("amogus.wav");
 
         File file = new File("images/bruh.png");
         BufferedImage bufferedImage = ImageIO.read(file);
@@ -28,15 +58,6 @@ public class amogus {
         // jLabel.setIcon(new ImageIcon(bufferedImage2));
 
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public static void changeImage(String image) throws IOException
-    {
-        String imageDirectory = "images/" + image;
-        File file = new File(imageDirectory);
-        BufferedImage bufferedImage = ImageIO.read(file);
-        ImageIcon imageIcon = new ImageIcon(bufferedImage);
-        jLabel.setIcon(imageIcon); 
     }
 }
 
