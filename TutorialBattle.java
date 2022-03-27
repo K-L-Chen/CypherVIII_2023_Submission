35cb8af4f0dbf48b6853e7af43e0e1d059cf6bde
klchen <klchen@email.wm.edu>
2022-03-26 21:50:50 -0400

    Put Battles in, boss battles have bugs

--- TutorialBattle.java
+++ TutorialBattle.java
@@ -2,20 +2,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TutorialBattle extends Battler{
-    Character player; 
-    ArrayList<Enemy> enemy_list = new ArrayList<Enemy>();
-    Scanner in;
+    //Character player; 
+    //ArrayList<Enemy> enemy_list = new ArrayList<Enemy>();
+    //Scanner in;
 
     public TutorialBattle(Character player_to_set, ArrayList<Enemy> enemy_to_set, Scanner in){
-        super(player_to_set, enemy_to_set, in);
-        this.player = player_to_set;
-        this.enemy_list = enemy_to_set;
-        say_lines = new ArrayList<String>();
-        say_lines.add("Prepare to be executed!");
-        say_lines.add("Time for a throw down!");
-        say_lines.add("*Computer Science Major attempts to explain why Python is superior to Java. He's wrong.*");
-        say_lines.add("Fine, I'll commit my all to this!");
-        say_lines.add("No exceptions!");
-        this.start_battle(in);
+        //super(player_to_set, enemy_to_set, in);
+        //this.player = player_to_set;
+        //this.enemy_list = enemy_to_set;
+        super.say_lines = new ArrayList<String>();
+        super.say_lines.add("Prepare to be executed!");
+        super.say_lines.add("Time for a throw down!");
+        super.say_lines.add("*Computer Science Major attempts to explain why Python is superior to Java. He's wrong.*");
+        super.say_lines.add("Fine, I'll commit my all to this!");
+        super.say_lines.add("No exceptions!");
+        super.start_battle(in);
     }
 }
