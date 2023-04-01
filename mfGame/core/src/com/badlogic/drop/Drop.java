970b05c07c971d108815cc56a85b522257e8db1e
Kyle Chen <78221899+K-L-Chen@users.noreply.github.com>
2023-04-01 01:59:55 -0400

    Attempt at implementing rectangle labelling

--- mfGame/core/src/com/badlogic/drop/Drop.java
+++ mfGame/core/src/com/badlogic/drop/Drop.java
@@ -1,5 +1,6 @@
 package com.badlogic.drop;
 
+import java.awt.*;
 import java.util.Iterator;
 
 import com.badlogic.gdx.ApplicationAdapter;
@@ -13,6 +14,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -34,9 +38,7 @@ public class Drop extends ApplicationAdapter {
    private Texture texture;
    private Sprite backgroundSprite;
    private SpriteBatch spriteBatch;
-   
-   
-   
+   private String message;
    
    float screenWidth = 1920;
    float screenHeight = 1080;
@@ -47,9 +49,10 @@ public class Drop extends ApplicationAdapter {
    @Override
    public void create() {
 	   
-	  font = new BitmapFont();   
+	  font = new BitmapFont();
       // load the images for the droplet and the bucket, 64x64 pixels each
- 
+	  font.getData().scale(10);
+	  
 	  texture = new Texture(Gdx.files.internal("textbox.png"));
 	  backgroundSprite =new Sprite(texture);
 	  
@@ -68,7 +71,11 @@ public class Drop extends ApplicationAdapter {
       
       camera.setToOrtho(false, screenWidth, screenHeight);
       batch = new SpriteBatch();
+      //Label Label = new Label();
+      //Label.LabelStyle style = new LabelStyle();
+      //style.background = ...; // Set the drawable you want to use
 
+      //Label label = new Label("Text here", style);
    }
 
    @Override
@@ -78,20 +85,35 @@ public class Drop extends ApplicationAdapter {
       // blue and alpha component in the range [0,1]
       // of the color to be used to clear the screen.
       ScreenUtils.clear(0, 0, 0.2f, 1);
-
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+      int swidth = (int)screenSize.getWidth();
+      int slength = (int)screenSize.getHeight();
       // tell the camera to update its matrices.
       camera.update();
-
+      System.out.println(swidth + ", " + slength);
+      
+      //use labelstyle to lock text to texture
+      LabelStyle style = new LabelStyle();
+      style.font = font;
+      style.fontColor = Color.RED;
+      message = "hello";
+      
+      style.background = new TextureRegionDrawable(new TextureRegion(texture));;
+      Label label = new Label(message, style);
+      
       // tell the SpriteBatch to render in the
       // coordinate system specified by the camera.
       batch.setProjectionMatrix(camera.combined);
-
+      //font.getData().scale(1);
+      
       // begin a new batch and draw the bucket and
       // all drops
       batch.begin();
-      batch.draw(texture, 0, 0, 1920, 540);
-//      font.setScale(.2f);
-      font.draw(batch, "hello", 500, 500);
+      batch.draw(texture, 0, 0, swidth, slength / 3); //1920, 540
+      //font.getData().scale(20);
+      font.setColor(Color.ORANGE);
+      font.draw(batch, message, 250, 250);
+      //font.draw(batch, "hello", swidth / 2, slength / 4, 0, 0, lastDropTime, 0, false);
       batch.end();
 
 
@@ -104,5 +126,11 @@ public class Drop extends ApplicationAdapter {
       dropSound.dispose();
       rainMusic.dispose();
       batch.dispose();
+   }
+   
+   //read a textfile and grab the next group of output text
+   public String readTextFile(String textfile) {
+	   message = "";
+	   return message;
    }
 }
