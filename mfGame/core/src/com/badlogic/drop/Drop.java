5c7ca61845606627ccdb26d74ddcdedfeab62e16
Kyle Chen <78221899+K-L-Chen@users.noreply.github.com>
2023-04-01 12:39:26 -0400

    Got text to look nice; Labels don't work without usage of Stage()

--- mfGame/core/src/com/badlogic/drop/Drop.java
+++ mfGame/core/src/com/badlogic/drop/Drop.java
@@ -30,28 +30,39 @@ public class Drop extends ApplicationAdapter {
    private Texture textboxImage;
    private Sound dropSound;
    private Music rainMusic;
+   private Sound mechaResLife;
    private SpriteBatch batch;
    private OrthographicCamera camera;
    private Rectangle textbox;
    private long lastDropTime;      
    private BitmapFont font;
    private Texture texture;
+   
+   //top half of the screen
+   private Texture bg;
+   private Texture fg;
+   
    private Sprite backgroundSprite;
    private SpriteBatch spriteBatch;
    private String message;
    
    float screenWidth = 1920;
    float screenHeight = 1080;
+   //inner box height and width
+   float[] ibWidth = new float[]{192,1728};
+   float[] ibHeight = new float[]{420,120};
+   //Label label;
    
    public void renderBackground() {
        backgroundSprite.draw(spriteBatch);
    }
+   
    @Override
    public void create() {
 	   
 	  font = new BitmapFont();
       // load the images for the droplet and the bucket, 64x64 pixels each
-	  font.getData().scale(10);
+	  font.getData().scale(5); //4,5 is good size
 	  
 	  texture = new Texture(Gdx.files.internal("textbox.png"));
 	  backgroundSprite =new Sprite(texture);
@@ -72,11 +83,17 @@ public class Drop extends ApplicationAdapter {
       
       camera.setToOrtho(false, screenWidth, screenHeight);
       batch = new SpriteBatch();
-      //Label Label = new Label();
-      //Label.LabelStyle style = new LabelStyle();
-      //style.background = ...; // Set the drawable you want to use
-
-      //Label label = new Label("Text here", style);
+      
+      //USING LABELS REQUIRES USE OF stage()
+      
+      //LabelStyle style = new LabelStyle();
+      //style.font = font;
+      //style.fontColor = Color.RED;
+      
+      //style.background = new TextureRegionDrawable(new TextureRegion(texture));
+      //label = new Label(message, style);
+      //label.setBounds(ibWidth[0], ibWidth[1], ibHeight[0], ibHeight[1]);
+      //label.setFontScale(5f);
    }
 
    @Override
@@ -86,22 +103,17 @@ public class Drop extends ApplicationAdapter {
       // blue and alpha component in the range [0,1]
       // of the color to be used to clear the screen.
       ScreenUtils.clear(0, 0, 0.2f, 1);
-      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+      
+      //active screen size update, depreciated as it doesn't change
+      //regardless of fullscreen or not
+      /*Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
       int swidth = (int)screenSize.getWidth();
-      int slength = (int)screenSize.getHeight();
-      // tell the camera to update its matrices.
+      int slength = (int)screenSize.getHeight();*/
+      //System.out.println(swidth + ", " + slength);
+      
+   // tell the camera to update its matrices.
       camera.update();
-      System.out.println(swidth + ", " + slength);
-      
-      //use labelstyle to lock text to texture
-      LabelStyle style = new LabelStyle();
-      style.font = font;
-      style.fontColor = Color.RED;
-      
-      style.background = new TextureRegionDrawable(new TextureRegion(texture));;
-      Label label = new Label(message, style);
-      label.setStyle(style);
-      
+            
       // tell the SpriteBatch to render in the
       // coordinate system specified by the camera.
       batch.setProjectionMatrix(camera.combined);
@@ -111,9 +123,10 @@ public class Drop extends ApplicationAdapter {
       // all drops	
       batch.begin();
       batch.draw(texture, 0, 0, screenWidth, screenHeight / 2);//swidth, slength / 3); //1920, 540
+      //batch.addActor(label);
       //font.getData().scale(20);
-      font.setColor(Color.ORANGE);
-      font.draw(batch, message, 5, slength / 4);
+      font.setColor(Color.RED);
+      font.draw(batch, message, ibWidth[0], ibHeight[0]);
       //font.draw(batch, "hello", swidth / 2, slength / 4, 0, 0, lastDropTime, 0, false);
       batch.end();
 
