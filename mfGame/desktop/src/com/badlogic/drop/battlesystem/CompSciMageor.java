e3c6562b8c8bc564f6d26ee4ca3cd5a4dd614a2a
Kyle Chen <78221899+K-L-Chen@users.noreply.github.com>
2023-04-01 20:43:12 -0400

    NUCLEAR OPTION 2
    Mutually Assured Bugfix

--- mfGame/desktop/src/com/badlogic/drop/battlesystem/CompSciMageor.java
+++ mfGame/desktop/src/com/badlogic/drop/battlesystem/CompSciMageor.java
@@ -9,16 +9,60 @@ public class CompSciMageor extends Battler {
 	@Override
 	public void attack(Battler Other)
 	{
-		
+		double totalmult = (this.attackMult - Other.defenceMult);
+		if(totalmult == 0)
+		{
+			totalmult = 0.1;
+		}
+		Other.curHp = (int) (Other.curHp - (10*totalmult));
+		if(Other.curHp < 0)
+		{
+			Other.isDefeated = true;
+		}
 	}
 	
 	@Override 
 	public void skill1(Battler Other) {
-
+		if(this.curMp < 10)
+		{
+			//Not enough MP
+		}
+		else
+		{
+			this.curMp = this.curMp-10;
+			this.curHp = this.curHp+10;
+			if(this.curHp > this.maxHp)
+			{
+				this.curHp = this.maxHp;
+			}
+		}
 	}
 	
 	@Override
 	public void skill2(Battler Other) {
-
+		if(this.curMp < 20)
+		{
+			//Not enough MP
+		}
+		else
+		{
+			this.curMp = this.curMp-20;
+			Other.attackMult = Other.attackMult/1.5;
+			Other.defenceMult = Other.defenceMult/1.5;
+		}
+	}
+	
+	@Override
+	public String getSkill1Name()
+	{
+		return "Healing Potion (Raw caffeine)";
+		
+	}
+	
+	@Override
+	public String getSkill2Name()
+	{
+		return "Hack'em";
+		
 	}
 }
--- mfGame/desktop/src/com/badlogic/drop/battlesystem/OpenSourceBanbit.java
+++ mfGame/desktop/src/com/badlogic/drop/battlesystem/OpenSourceBanbit.java
@@ -9,16 +9,59 @@ public class OpenSourceBanbit extends Battler {
 	@Override
 	public void attack(Battler Other)
 	{
-		
+		double totalmult = (this.attackMult - Other.defenceMult);
+		if(totalmult == 0)
+		{
+			totalmult = 0.1;
+		}
+		Other.curHp = (int) (Other.curHp - (10*totalmult));
+		if(Other.curHp < 0)
+		{
+			Other.isDefeated = true;
+		}
 	}
 	
 	@Override 
 	public void skill1(Battler Other) {
-
+		double totalmult = (this.attackMult - Other.defenceMult);
+		if(totalmult == 0)
+		{
+			totalmult = 0.1;
+		}
+		Other.curHp = (int) (Other.curHp - (10*totalmult));
+		Other.defenceMult = Other.defenceMult/1.5;
+		if(Other.curHp < 0)
+		{
+			Other.isDefeated = true;
+		}
 	}
 	
 	@Override
 	public void skill2(Battler Other) {
-
+		double totalmult = (this.attackMult - Other.defenceMult);
+		if(totalmult == 0)
+		{
+			totalmult = 0.1;
+		}
+		Other.curHp = (int) (Other.curHp - 3*(10*totalmult));
+		Other.defenceMult = Other.defenceMult/1.5;
+		if(Other.curHp < 0)
+		{
+			Other.isDefeated = true;
+		}
+	}
+	
+	@Override
+	public String getSkill1Name()
+	{
+		return "Go-N-Back";
+		
+	}
+	
+	@Override
+	public String getSkill2Name()
+	{
+		return "Multi-Threaded Strike";
+		
 	}
 }
